687530534c1dd12b017d5483a0862b60d423b570
kenchan1979 <saimyonkk@gmail.com>
2022-01-20 20:51:30 +0900

    slot with name

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,3 +1,4 @@
 <x-tests.app>
+    <x-slot name="header">header1</x-slot>
 コンポーネントテスト１
 </x-tests.app>
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,4 +1,5 @@
 <x-tests.app>
+    <x-slot name="header">header2</x-slot>
     コンポーネントテスト2
     </x-tests.app>
     
