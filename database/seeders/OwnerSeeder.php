275518e8cd66c15dc6325ce86aed1bc6e16e3fe8
kenchan1979 <saimyonkk@gmail.com>
2022-01-25 19:14:14 +0900

    Carbon

---
+++ database/seeders/OwnerSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class OwnerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('owners')->insert([
+            [
+            'name' => 'ken1',
+            'email' => 'kensuga1@gmail.com',
+            'password' => Hash::make('password123'),
+            'created_at' => '2022/01/01 11:11:11'
+            ],
+            [
+                'name' => 'ken2',
+                'email' => 'kensuga2@gmail.com',
+                'password' => Hash::make('password123'),
+                'created_at' => '2022/01/01 11:11:11'
+            ],
+            [
+                'name' => 'ken3',
+                'email' => 'kensuga3@gmail.com',
+                'password' => Hash::make('password123'),
+                'created_at' => '2022/01/01 11:11:11'
+            ],
+        ]);
+    }
+}
