b8b42bee0ccf72c5d022cd59463ac6911c6cf7e6
kenchan1979 <saimyonkk@gmail.com>
2022-02-04 21:38:55 +0900

    destroy

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -228,6 +228,11 @@ class ProductController extends Controller
      */
     public function destroy($id)
     {
-        //
+        Product::findOrFail($id)->delete(); //ソフトデリート    
+
+        return redirect()
+        ->route('owner.products.index')
+        ->with(['message' => '商品を削除しました。',
+        'status' => 'alert']);
     }
 }
