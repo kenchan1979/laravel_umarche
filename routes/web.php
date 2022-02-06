33f371dab1919ccb78f1331961ed06c1bd259323
kenchan1979 <saimyonkk@gmail.com>
2022-02-06 16:43:18 +0900

    cartIndex

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,6 @@ use App\Http\Controllers\LifeCycleTestController;
 use App\Http\Controllers\User\ItemController;
 use App\Http\Controllers\User\CartController;
 
-
-
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -21,20 +18,22 @@ use App\Http\Controllers\User\CartController;
 */
 
 Route::get('/', function () {
-    return view('user/welcome');
+    return view('user.welcome');
 });
 
 Route::middleware('auth:users')->group(function(){
-        Route::get('/', [ItemController::class,'index'])->name('items.index');
-        Route::get('show/{item}',[ItemController::class, 'show'])->name('items.show');
+        Route::get('/', [ItemController::class, 'index'])->name('items.index');
+        Route::get('show/{item}', [ItemController::class, 'show'])->name('items.show');
 });
 
 Route::prefix('cart')->middleware('auth:users')->group(function(){
-        Route::post('add', [CartController::class,'add'])->name('cart.add');
+        Route::get('/', [CartController::class, 'index'])->name('cart.index');
+        Route::post('add', [CartController::class, 'add'])->name('cart.add');   
+        Route::post('delete/{item}', [CartController::class, 'delete'])->name('cart.delete');
 });
 
 // Route::get('/dashboard', function () {
-//     return view('user/dashboard');
+//     return view('user.dashboard');
 // })->middleware(['auth:users'])->name('dashboard');
 
 Route::get('/component-test1', [ComponentTestController::class, 'showComponent1']);
@@ -42,6 +41,4 @@ Route::get('/component-test2', [ComponentTestController::class, 'showComponent2'
 Route::get('/servicecontainertest', [LifeCycleTestController::class, 'showServiceContainerTest']);
 Route::get('/serviceprovidertest', [LifeCycleTestController::class, 'showServiceProviderTest']);
 
-
-
-require __DIR__ . '/auth.php';
+require __DIR__.'/auth.php';
